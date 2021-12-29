cce4bf117764ec5105d26092c4e565d6f804e38e
Will Roden <will@roden.cc>
2021-12-29 11:26:53 -0600

    go env

--- windows/test.bash
+++ windows/test.bash
@@ -29,6 +29,8 @@ PATH="${DOWNLOAD_CACHE}/protoc/bin:${PATH}"
 
 set -x
 
+go env
+
 go install google.golang.org/protobuf/cmd/protoc-gen-go@${PROTOC_GEN_GO_VERSION}
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@${PROTOC_GEN_GO_GRPC_VERSION}
 go install ./private/buf/cmd/buf/command/protoc/internal/protoc-gen-insertion-point-writer
